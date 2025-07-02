37636c0b60742dfcfb213a2fd337dd8eb05ae1bb
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-02 14:47:01 +0300

    home blade change

--- resources/views/layouts/checkout.blade.php
+++ resources/views/layouts/checkout.blade.php
@@ -1,39 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-    >
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Demo Storefront</title>
-    <meta
-        name="description"
-        content="Example of an ecommerce storefront built with Lunar."
-    >
-    =
-    @vite('resources/css/app.css')
-    <link
-        rel="icon"
-        href="{{ asset('favicon.svg') }}"
-    >
+    <meta name="description" content="Example of an ecommerce storefront built with Lunar.">
+    <link rel="icon" href="{{ asset('favicon.svg') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
-
-<body class="antialiased text-gray-900">
+<body class="flex flex-col min-h-screen antialiased text-gray-900">
 @livewire('components.navigation')
-<a href="{{ route('lang.switch', ['lang' => 'en']) }}">English</a>
-<a href="{{ route('lang.switch', ['lang' => 'uk']) }}">Українська</a>
-
-
 <main>
     {{ $slot }}
 </main>
-
-<x-footer/>
-
 @livewireScripts
+<x-footer/>
 </body>
-
 </html>
