a8f3bde0e8b319aa289cb469e9c7732accebc86f
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-02 22:24:56 +0300

    home blade change

--- resources/views/livewire/blog-post-page.blade.php
+++ resources/views/livewire/blog-post-page.blade.php
@@ -1,16 +1,18 @@
 <div class="container mx-auto px-4 py-6">
-    <article class="max-w-3xl  mx-auto bg-white rounded-2xl shadow-sm p-6 flex flex-col gap-6">
+    <article class="max-w-3xl mx-auto bg-white rounded-2xl shadow-sm p-6 flex flex-col gap-6">
         <!-- Header -->
         <header class="flex flex-col gap-2">
             <h1 class="text-3xl font-bold text-zinc-800">
                 {{ $post->getTranslation('title', app()->getLocale()) }}
             </h1>
             <div class="flex items-center gap-2 text-sm text-neutral-400">
-                <time datetime="{{ $post->published_at->format('Y-m-d') }}">
-                    {{ $post->published_at->format('d.m.Y') }}
-                </time>
-                <div class="h-1 w-1 bg-neutral-400 rounded-full"></div>
-                <span>{{ __('messages.breadcrumbs.blog') }}</span>
+                @if ($post->published_at)
+                    <time datetime="{{ $post->published_at->format('Y-m-d') }}">
+                        {{ $post->published_at->format('d.m.Y') }}
+                    </time>
+                    <div class="h-1 w-1 bg-neutral-400 rounded-full"></div>
+                @endif
+                <span>{{ __('breadcrumbs.blog') }}</span>
             </div>
             <hr class="border-zinc-200">
         </header>
