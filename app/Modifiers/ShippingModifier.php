625ca1daf78b8430d33a0c4c380ac8857a57c85b
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-02 14:46:31 +0300

    home blade change

--- app/Modifiers/ShippingModifier.php
+++ app/Modifiers/ShippingModifier.php
@@ -8,7 +8,7 @@ use Lunar\Base\ShippingModifier as BaseShippingModifier;
 use Lunar\DataTypes\Price;
 use Lunar\DataTypes\ShippingOption;
 use Lunar\Facades\ShippingManifest;
-use Lunar\Models\Cart;
+use Lunar\Models\Contracts\Cart; // Use the interface
 use Lunar\Models\TaxClass;
 
 class ShippingModifier extends BaseShippingModifier
@@ -17,21 +17,25 @@ class ShippingModifier extends BaseShippingModifier
     {
         Log::debug('Обработка ShippingModifier', ['cart_id' => $cart->id]);
 
+        // Check if currency is set
         if (!$cart->currency) {
             Log::error('Валюта корзины не определена', ['cart_id' => $cart->id]);
             return $next($cart);
         }
 
+        // Check if shipping tables are enabled
         if (config('shipping-tables.enabled', false)) {
             Log::debug('Таблицы доставки включены, пропускаем кастомные опции');
             return $next($cart);
         }
 
+        // Get or create default tax class
         $taxClass = TaxClass::getDefault() ?? TaxClass::firstOrCreate(
             ['name' => 'Default'],
             ['created_at' => now(), 'updated_at' => now()]
         );
 
+        // Add shipping options
         ShippingManifest::addOptions(collect([
             new ShippingOption(
                 name: 'Самовивіз з магазину',
@@ -45,14 +49,14 @@ class ShippingModifier extends BaseShippingModifier
                 name: 'Нова Пошта',
                 description: 'Доставка на відділення Нової Пошти.',
                 identifier: 'nova-poshta',
-                price: new Price(0, $cart->currency, 1), // 👈 Цена 0
+                price: new Price(0, $cart->currency, 1),
                 taxClass: $taxClass
             ),
             new ShippingOption(
                 name: 'Кур’єрська доставка',
                 description: 'Доставка кур’єром за адресою.',
                 identifier: 'courier',
-                price: new Price(0, $cart->currency, 1), // 👈 Цена 0
+                price: new Price(0, $cart->currency, 1),
                 taxClass: $taxClass
             ),
         ]));
