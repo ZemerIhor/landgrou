40e24a61791a8419dc1b78eab35fdc9368e99216
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-08 01:19:01 +0300

    home blade change

--- app/Filament/Pages/Contacts.php
+++ app/Filament/Pages/Contacts.php
@@ -13,8 +13,10 @@ use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Pages\Page;
 use SolutionForest\FilamentTranslateField\Forms\Component\Translate;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
-class Contacts extends Page
+class Contacts extends Page implements HasForms
 {
     use InteractsWithForms;
 
@@ -35,6 +37,7 @@ class Contacts extends Page
     {
         $settings = app(ContactSettings::class);
 
+        // Инициализация данных формы из настроек
         $this->data = [
             'main_address' => $settings->main_address ?? ['en' => '', 'uk' => ''],
             'main_email' => $settings->main_email ?? '',
@@ -59,6 +62,28 @@ class Contacts extends Page
             ->schema([
                 Section::make(__('messages.contacts.title'))
                     ->schema([
+                        // Поля без переводов (email и телефон)
+                        TextInput::make('main_email')
+                            ->label(__('messages.contacts.main_email'))
+                            ->email()
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('sales_email')
+                            ->label(__('messages.contacts.sales_email'))
+                            ->email()
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('export_email')
+                            ->label(__('messages.contacts.export_email'))
+                            ->email()
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('export_phone')
+                            ->label(__('messages.contacts.export_phone'))
+                            ->tel()
+                            ->required()
+                            ->maxLength(20),
+                        // Поля с переводами
                         Translate::make()
                             ->locales(['en', 'uk'])
                             ->schema([
@@ -66,67 +91,52 @@ class Contacts extends Page
                                     ->label(__('messages.contacts.address'))
                                     ->required()
                                     ->maxLength(500),
-                                TextInput::make('main_email')
-                                    ->label(__('messages.contacts.main_email'))
-                                    ->email()
-                                    ->required()
-                                    ->maxLength(255),
-                                Repeater::make('sales_phones')
-                                    ->label(__('messages.contacts.sales_phones'))
-                                    ->schema([
-                                        TextInput::make('phone')
-                                            ->label(__('messages.contacts.phone'))
-                                            ->required()
-                                            ->tel()
-                                            ->maxLength(20),
-                                    ])
-                                    ->collapsible()
-                                    ->cloneable(),
-                                TextInput::make('sales_email')
-                                    ->label(__('messages.contacts.sales_email'))
-                                    ->email()
-                                    ->required()
-                                    ->maxLength(255),
-                                TextInput::make('export_phone')
-                                    ->label(__('messages.contacts.export_phone'))
-                                    ->tel()
-                                    ->required()
-                                    ->maxLength(20),
                                 TextInput::make('export_contact')
                                     ->label(__('messages.contacts.export_contact'))
                                     ->required()
                                     ->maxLength(255),
-                                TextInput::make('export_email')
-                                    ->label(__('messages.contacts.export_email'))
-                                    ->email()
-                                    ->required()
-                                    ->maxLength(255),
-                                Repeater::make('additional_emails')
-                                    ->label(__('messages.contacts.additional_emails'))
-                                    ->schema([
-                                        TextInput::make('key')
-                                            ->label(__('messages.contacts.email_key'))
-                                            ->required()
-                                            ->maxLength(50),
-                                        TextInput::make('value')
-                                            ->label(__('messages.contacts.email_value'))
-                                            ->email()
-                                            ->required()
-                                            ->maxLength(255),
-                                    ])
-                                    ->collapsible()
-                                    ->cloneable(),
-                                FileUpload::make('map_image')
-                                    ->label(__('messages.contacts.map_image'))
-                                    ->image()
-                                    ->disk('public')
-                                    ->directory('contacts-images')
-                                    ->required(),
                                 TextInput::make('map_image_alt')
                                     ->label(__('messages.contacts.map_alt'))
                                     ->required()
                                     ->maxLength(255),
                             ]),
+                        // Repeater для телефонов
+                        Repeater::make('sales_phones')
+                            ->label(__('messages.contacts.sales_phones'))
+                            ->schema([
+                                TextInput::make('phone')
+                                    ->label(__('messages.contacts.phone'))
+                                    ->required()
+                                    ->tel()
+                                    ->maxLength(20),
+                            ])
+                            ->collapsible()
+                            ->cloneable(),
+                        // Repeater для дополнительных email
+                        Repeater::make('additional_emails')
+                            ->label(__('messages.contacts.additional_emails'))
+                            ->schema([
+                                TextInput::make('key')
+                                    ->label(__('messages.contacts.email_key'))
+                                    ->required()
+                                    ->maxLength(50),
+                                TextInput::make('value')
+                                    ->label(__('messages.contacts.email_value'))
+                                    ->email()
+                                    ->required()
+                                    ->maxLength(255),
+                            ])
+                            ->collapsible()
+                            ->cloneable(),
+                        // Поле для изображения карты
+                        FileUpload::make('map_image')
+                            ->label(__('messages.contacts.map_image'))
+                            ->image()
+                            ->disk('public')
+                            ->directory('contacts-images')
+                            ->accept(['image/jpeg', 'image/png', 'image/gif', 'image/webp'])
+                            ->rules(['nullable', 'image', 'mimes:jpeg,png,jpg,gif,webp'])
+                            ->required(),
                     ])
                     ->collapsible(),
             ])
@@ -138,10 +148,15 @@ class Contacts extends Page
         try {
             $data = $this->form->getState();
 
-            // Transform additional_emails into an associative array
+            // Логирование MIME-типа для map_image
+            if (isset($data['map_image']) && is_object($data['map_image'])) {
+                Log::info('MIME type for map_image', ['mime' => $data['map_image']->getMimeType()]);
+            }
+
+            // Преобразование additional_emails в ассоциативный массив
             $data['additional_emails'] = collect($data['additional_emails'])->pluck('value', 'key')->toArray();
 
-            \Illuminate\Support\Facades\Log::info('Contact Settings Form Data', ['data' => $data]);
+            Log::info('Contact Settings Form Data', ['data' => $data]);
 
             $settings = app(ContactSettings::class);
             $settings->fill($data);
@@ -151,8 +166,23 @@ class Contacts extends Page
                 ->title(__('messages.contacts.saved'))
                 ->success()
                 ->send();
+        } catch (ValidationException $e) {
+            Log::error('Помилки валідації в налаштуваннях контактів', [
+                'errors' => $e->errors(),
+                'message' => $e->getMessage(),
+            ]);
+
+            Notification::make()
+                ->title(__('messages.contacts.error'))
+                ->body(implode(', ', array_merge(...array_values($e->errors()))))
+                ->danger()
+                ->send();
         } catch (\Exception $e) {
-            \Illuminate\Support\Facades\Log::error('Error saving contact settings', ['error' => $e->getMessage()]);
+            Log::error('Помилка збереження налаштувань контактів', [
+                'error' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+
             Notification::make()
                 ->title(__('messages.contacts.error'))
                 ->body($e->getMessage())
