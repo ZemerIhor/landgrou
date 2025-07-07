a4a0f53c7f3d38188e3dd56780551d02920b6bb3
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-08 01:37:43 +0300

    home blade change

--- app/Settings/ContactSettings.php
+++ app/Settings/ContactSettings.php
@@ -10,18 +10,18 @@ class ContactSettings extends Settings
     use HasTranslations;
 
     // Переводимые поля
-    public array $main_address;
-    public array $export_contact;
-    public array $map_image_alt;
+    public  $main_address;
+    public  $export_contact;
+    public  $map_image_alt;
 
     // Непереводимые поля
-    public string $main_email;
-    public array $sales_phones;
-    public string $sales_email;
-    public string $export_phone;
-    public string $export_email;
-    public array $additional_emails;
-    public string $map_image;
+    public  $main_email;
+    public  $sales_phones;
+    public  $sales_email;
+    public  $export_phone;
+    public  $export_email;
+    public  $additional_emails;
+    public ?string  $map_image;
 
     // Публичное свойство для переводимых полей
     public array $translatable = [
