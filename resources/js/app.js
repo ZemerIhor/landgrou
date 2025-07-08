9638729ef3d969217343b7ebc69e976644efd891
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-08 21:15:51 +0300

    home blade change

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,23 @@
-import Swiper from 'swiper';
-window.Swiper = Swiper; // Делаем Swiper доступным глобально, если нужно
+document.addEventListener("DOMContentLoaded", initApp);
+document.addEventListener("livewire:navigated", initApp);
+document.addEventListener("livewire:init", initApp);
+document.addEventListener("livewire:update", initApp);
 
-document.querySelectorAll('[data-toggle]').forEach(button => {
-    button.addEventListener('click', () => {
-        const targetId = button.getAttribute('data-toggle');
-        const answer = document.getElementById(targetId);
-        const isExpanded = button.getAttribute('aria-expanded') === 'true';
+function initApp () {
+    import Swiper from 'swiper';
+    window.Swiper = Swiper; // Делаем Swiper доступным глобально, если нужно
 
-        answer.classList.toggle('max-h-0');
-        answer.classList.toggle('max-h-96');
-        button.setAttribute('aria-expanded', !isExpanded);
+    document.querySelectorAll('[data-toggle]').forEach(button => {
+        button.addEventListener('click', () => {
+            const targetId = button.getAttribute('data-toggle');
+            const answer = document.getElementById(targetId);
+            const isExpanded = button.getAttribute('aria-expanded') === 'true';
+
+            answer.classList.toggle('max-h-0');
+            answer.classList.toggle('max-h-96');
+            button.setAttribute('aria-expanded', !isExpanded);
+        });
     });
-});
+}
+
 
