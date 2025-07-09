7c432e9b731941f7963db34a3a00a18c5ba2aa3c
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-10 01:05:38 +0300

    home blade change

--- app/Settings/GlobalSettings.php
+++ app/Settings/GlobalSettings.php
@@ -6,15 +6,15 @@ use Spatie\LaravelSettings\Settings;
 
 class GlobalSettings extends Settings
 {
-    public array $site_name;
-    public array $meta_description;
-    public string $logo;
-    public string $favicon;
-    public string $contact_email;
-    public array $feedback_form_title;
-    public array $feedback_form_description;
-    public string $feedback_form_image;
-    public array $feedback_form_image_alt;
+    public  $site_name;
+    public  $meta_description;
+    public  $logo;
+    public  $favicon;
+    public  $contact_email;
+    public  $feedback_form_title;
+    public  $feedback_form_description;
+    public  $feedback_form_image;
+    public  $feedback_form_image_alt;
 
     public static function group(): string
     {
