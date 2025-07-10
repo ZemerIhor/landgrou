810c3b596e440f72f2e58a295cb86a785eb175aa
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-11 01:20:23 +0300

    home blade change

--- resources/views/layouts/storefront.blade.php
+++ resources/views/layouts/storefront.blade.php
@@ -3,19 +3,34 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Demo Storefront</title>
-    <meta name="description" content="Example of an ecommerce storefront built with Lunar.">
-    <link rel="icon" href="{{ asset('favicon.svg') }}">
+
+    @php
+        $settings = app(\App\Settings\GlobalSettings::class);
+        $locale = app()->getLocale();
+    @endphp
+
+    <title>{{ $settings->site_name[$locale] ?? __('messages.settings.default_site_name') }}</title>
+    <meta name="description" content="{{ $settings->meta_description[$locale] ?? __('messages.settings.default_meta_description') }}">
+
+    @if ($settings->favicon)
+        <link rel="icon" href="{{ Storage::url($settings->favicon) }}">
+    @else
+        <link rel="icon" href="{{ asset('favicon.svg') }}">
+    @endif
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
 <body class="flex flex-col min-h-screen antialiased text-gray-900">
 @livewire('components.navigation')
+
 <main>
     {{ $slot }}
 </main>
+
 @livewireScripts
 @stack('scripts')
+
 <x-footer/>
 </body>
 </html>
