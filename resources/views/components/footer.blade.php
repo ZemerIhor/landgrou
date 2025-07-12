dca3e53a50e366ac04fe7757a920305fa1eaf25f
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-12 23:24:36 +0300

    home blade change

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -85,20 +85,6 @@
             <p class="copyright">
                 {{ is_array($footer->copyright_text) ? ($footer->copyright_text[app()->getLocale()] ?? $footer->copyright_text['en'] ?? '© All rights reserved') : ($footer->copyright_text ?? '© All rights reserved') }}
             </p>
-            <button
-                type="button"
-                aria-label="{{ __('messages.footer.scroll_to_top') }}"
-                class="scroll-to-top"
-                onclick="window.scrollTo({top: 0, behavior: 'smooth'})"
-            >
-                <div class="scroll-icon">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                         xmlns="http://www.w3.org/2000/svg" class="icon">
-                        <path d="M7 14L12 9L17 14" stroke="currentColor" stroke-width="2"
-                              stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div>
-            </button>
 
         </section>
 
