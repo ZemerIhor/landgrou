dbd07c9532ec1c28ed006d3b0bf3189ad31f0346
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-13 15:28:24 +0300

    home blade change

--- resources/views/livewire/components/navigation.blade.php
+++ resources/views/livewire/components/navigation.blade.php
@@ -84,7 +84,7 @@
             </style>
 
             <!-- Desktop Menu -->
-            <div class="desktop-menu hidden  sm:flex ">
+            <div class="desktop-menu sm:flex hidden ">
                 @if(app()->getLocale() === 'en')
                     <x-filament-menu-builder::menu slug="en-header-menu" />
                 @elseif(app()->getLocale() === 'uk')
