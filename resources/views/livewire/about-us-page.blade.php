672585d9331b5bbd7a61dd325f1e43fb3c09802f
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-17 05:16:07 +0300

    home blade change

--- resources/views/livewire/about-us-page.blade.php
+++ resources/views/livewire/about-us-page.blade.php
@@ -4,6 +4,9 @@
 
 <div class="max-w-full mx-auto">
     <div class="top-section relative">
+        <img src="{{ asset('images/1.png') }}" alt="" class="object-contain self-stretch my-auto  max-md:max-w-full absolute left-0 bottom-0" />
+        <img src="{{ asset('images/2.png') }}" alt="" class="object-contain self-stretch my-auto  max-md:max-w-full absolute right-0 bottom-0" />
+
         <header class="font-semibold">
             <div class="flex relative flex-col items-center pb-20 w-full min-h-[780px] max-md:px-5 max-md:max-w-full">
                 <!-- Фоновое изображение -->
@@ -153,8 +156,6 @@
             </section>
             <section class="flex overflow-hidden relative flex-col items-center px-12 py-20 w-full max-w-screen-xl max-md:px-5 max-md:max-w-full">
                 <!-- Background Pattern -->
-                <img src="{{ asset('images/1.png') }}" alt="" class="object-contain self-stretch my-auto  max-md:max-w-full absolute left-0" />
-                <img src="{{ asset('images/2.png') }}" alt="" class="object-contain self-stretch my-auto  max-md:max-w-full absolute right-0" />
 
                 <!-- Content -->
                 <div class="z-0 max-w-full text-center text-zinc-100 w-[779px]">
