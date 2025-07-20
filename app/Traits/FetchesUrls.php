207e3e5a2092a86a1897deb67ed716921184b61f
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-21 00:46:34 +0300

    dawdadw

--- app/Traits/FetchesUrls.php
+++ app/Traits/FetchesUrls.php
@@ -2,19 +2,27 @@
 
 namespace App\Traits;
 
-use Lunar\Models\Url as LunarUrl;
+use Lunar\Models\Url;
 
 trait FetchesUrls
 {
-    public function fetchUrl($slug, $elementType, $with = []): ?LunarUrl
+    /**
+     * The URL model from the slug.
+     */
+    public ?Url $url = null;
+
+    /**
+     * Fetch a url model.
+     *
+     * @param  string  $slug
+     * @param  string  $type
+     * @param  array  $eagerLoad
+     */
+    public function fetchUrl($slug, $type, $eagerLoad = []): ?Url
     {
-        $query = LunarUrl::where('element_type', $elementType)
-            ->whereIn('slug', [$slug, $slug . 'vfv', str_replace('vfv', '', $slug)]);
-
-        if (!empty($with)) {
-            $query->with($with);
-        }
-
-        return $query->first();
+        return Url::whereElementType($type)
+            ->whereDefault(true)
+            ->whereSlug($slug)
+            ->with($eagerLoad)->first();
     }
 }
