096b2a14d12ac90924321b27eedda29139571431
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-20 23:22:36 +0300

    dawdadw

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Livewire\SearchPage;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\App;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 // Явное переключение языка с редиректом
 Route::get('/lang/{locale}', function ($locale) {
@@ -43,7 +44,10 @@ Route::get('/switch/{locale}', function ($locale) {
 })->name('lang.quick_switch');
 
 // Группа маршрутов с префиксом языка (опционально)
-Route::group(['prefix' => '{locale?}', 'middleware' => ['localization']], function () {
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['localize', 'localizationRedirect', 'localeSessionRedirect']
+], function () {
     Route::get('/', Home::class)->name('home');
     Route::get('/catalog', CatalogPage::class)->name('catalog.view');
     Route::get('/reviews', \App\Livewire\ReviewsPage::class)->name('reviews');
