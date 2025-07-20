8601924373caa7cde37d74d6da72265baf707b92
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-07-20 13:08:23 +0300

    home blade change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -52,9 +52,6 @@ class AppServiceProvider extends ServiceProvider
                 ])
                 ->plugins([
                     new ShippingPlugin,
-                    FilamentMenuBuilderPlugin::make()
-                        ->addLocation('header', 'Header') // Локация для основного меню
-                        ->showCustomLinkPanel(true),
                     FilamentorPlugin::make(),
                     FilamentTranslateFieldPlugin::make()
                         ->defaultLocales(['en', 'uk'])
