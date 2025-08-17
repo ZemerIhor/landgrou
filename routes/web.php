80c043fea844286581ece4338566384411373666
Vlad Koltyshev <koltishev.1998@gmail.com>
2025-08-17 13:08:17 +0300

    dd

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,57 @@
 <?php
 
+use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\PageController;
 use App\Livewire\AboutUsPage;
 use App\Livewire\BlogPage;
-use App\Livewire\CheckoutPage;
-use App\Livewire\CheckoutSuccessPage;
+use App\Livewire\BlogPostPage;
+use App\Livewire\CatalogPage;
+use App\Livewire\CollectionPage;
 use App\Livewire\ContactsPage;
 use App\Livewire\FaqPage;
 use App\Livewire\Home;
+use App\Livewire\ProductPage;
 use App\Livewire\ReviewsPage;
-use App\Livewire\SubmitReview;
 use App\Livewire\SearchPage;
+use App\Livewire\SubmitReview;
+use App\Livewire\CheckoutPage;
+use App\Livewire\CheckoutSuccessPage;
 use Illuminate\Support\Facades\Route;
-use Lunar\Hub\Http\Livewire\UrlResolver;
 
-// 🔄 Переключение языка
-Route::get('/lang/{locale}', [\App\Http\Controllers\LanguageController::class, 'switch'])->name('lang.switch');
-Route::get('/switch/{locale}', [\App\Http\Controllers\LanguageController::class, 'quickSwitch'])->name('lang.quick_switch');
+// 🔄 Переключение языка (вне группы локали)
+Route::get('/lang/{locale}', [LanguageController::class, 'switch'])->name('lang.switch');
+Route::get('/switch/{locale}', [LanguageController::class, 'quickSwitch'])->name('lang.quick_switch');
 
-// 🌍 Группа маршрутов с локалью
-Route::group(['prefix' => '{locale?}', 'middleware' => ['localization']], function () {
-    // Статические страницы
+// 🌍 Группа маршрутов с префиксом локали
+Route::group([
+    'prefix' => '{locale?}',
+    'where'  => ['locale' => 'en|uk'], // подставь свои коды языков
+    'middleware' => ['localization'],
+], function () {
+    // Статика / разделы
     Route::get('/', Home::class)->name('home');
-    Route::get('/catalog', \App\Livewire\CatalogPage::class)->name('catalog.view');
+    Route::get('/catalog', CatalogPage::class)->name('catalog.view');
     Route::get('/reviews', ReviewsPage::class)->name('reviews');
     Route::get('/submit-review', SubmitReview::class)->name('submit-review');
-    Route::get('/privacy-policy', [\App\Http\Controllers\PageController::class, 'privacyPolicy'])->name('privacy-policy');
-    Route::get('/terms', [\App\Http\Controllers\PageController::class, 'terms'])->name('terms');
+    Route::get('/privacy-policy', [PageController::class, 'privacyPolicy'])->name('privacy-policy');
+    Route::get('/terms', [PageController::class, 'terms'])->name('terms');
     Route::get('/faq', FaqPage::class)->name('faq');
     Route::get('/about-us', AboutUsPage::class)->name('about-us');
     Route::get('/contacts', ContactsPage::class)->name('contacts');
+
+    // Блог
     Route::get('/blog', BlogPage::class)->name('blog.index');
+    Route::get('/blog/{slug}', BlogPostPage::class)->name('blog.post');
+
+    // Поиск / каталог
     Route::get('/search', SearchPage::class)->name('search.view');
+
+    // Продукты и коллекции (slug-и из таблицы lunar_urls)
+    Route::get('/products', SearchPage::class)->name('products.index');
+    Route::get('/products/{slug}', ProductPage::class)->name('product.view');
+    Route::get('/collections/{slug}', CollectionPage::class)->name('collection.view');
+
+    // Чекаут
     Route::get('/checkout', CheckoutPage::class)->name('checkout.view');
     Route::get('/checkout/success', CheckoutSuccessPage::class)->name('checkout-success.view');
-
-    // 🪄 Универсальный роут для продуктов / коллекций / динамических страниц
-    Route::get('/{slug}', UrlResolver::class)->name('url.resolver');
 });
