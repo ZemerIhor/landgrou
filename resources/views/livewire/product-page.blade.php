1adfb73eccbf4d90b96b278dfea5e7840e0608b9
Koltyshev Vladislav <koltishev.1998@gmail.com>
2026-01-04 18:36:22 +0200

    dd

--- resources/views/livewire/product-page.blade.php
+++ resources/views/livewire/product-page.blade.php
@@ -184,7 +184,7 @@
                              aria-label="{{ __('messages.product.quantity_selection') }}">
                             <button wire:click="incrementQuantity"
                                     class="flex relative gap-2.5 items-center"
-                                    aria-label="{{ __('messages.product.increment_quantity') }}">
+                                    aria-label="{{ __('messages.product.increment_quantity') }}">dddd
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="plus-icon">
                                     <path d="M12.75 7C12.75 6.58579 12.4142 6.25 12 6.25C11.5858 6.25 11.25 6.58579 11.25 7L11.25 11.25H7C6.58579 11.25 6.25 11.5858 6.25 12C6.25 12.4142 6.58579 12.75 7 12.75H11.25V17C11.25 17.4142 11.5858 17.75 12 17.75C12.4142 17.75 12.75 17.4142 12.75 17L12.75 12.75H17C17.4142 12.75 17.75 12.4142 17.75 12C17.75 11.5858 17.4142 11.25 17 11.25H12.75V7Z" fill="#333333"/>
                                 </svg>
